57365fc563f3588737de0ecfcdb523f32b22c8c4
neonekiz <gugatamiozzo@gmail.com>
2025-01-29 17:56:04 -0400

    release!

---
+++ src/main/java/net/neonekiz/douglasbirthdaymod/item/ModArmorMaterials.java
@@ -0,0 +1,86 @@
+package net.neonekiz.douglasbirthdaymod.item;
+
+import net.minecraft.Util;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.util.LazyLoadedValue;
+import net.minecraft.util.StringRepresentable;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.ArmorMaterials;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.ItemLike;
+
+import java.util.EnumMap;
+import java.util.function.Supplier;
+
+public enum ModArmorMaterials implements ArmorMaterial {
+
+
+    BDAYHAT("bdayhat", 5, (EnumMap)Util.make(new EnumMap(ArmorItem.Type.class), (p_266655_) -> {
+        p_266655_.put(ArmorItem.Type.HELMET, 1);
+    }), 0, SoundEvents.WOOL_PLACE, 0F, 0F, () -> Ingredient.of(new ItemLike[]{
+        Items.RED_WOOL}));
+
+    public static final StringRepresentable.EnumCodec<ArmorMaterials> CODEC = StringRepresentable.fromEnum(ArmorMaterials::values);
+    private static final EnumMap<ArmorItem.Type, Integer> HEALTH_FUNCTION_FOR_TYPE = (EnumMap) Util.make(new EnumMap(ArmorItem.Type.class), (p_266653_) -> {
+        p_266653_.put(ArmorItem.Type.HELMET, 11);
+    });
+    private final String name;
+    private final int durabilityMultiplier;
+    private final EnumMap<ArmorItem.Type, Integer> protectionFunctionForType;
+    private final int enchantmentValue;
+    private final SoundEvent sound;
+    private final float toughness;
+    private final float knockbackResistance;
+    private final LazyLoadedValue<Ingredient> repairIngredient;
+
+    private ModArmorMaterials(String pName, int pDurabilityMultiplier, EnumMap<ArmorItem.Type, Integer> pProtectionFunctionForType, int pEnchantmentValue, SoundEvent pSound, float pToughness, float pKnockbackResistance, Supplier<Ingredient> pRepairIngredient) {
+        this.name = pName;
+        this.durabilityMultiplier = pDurabilityMultiplier;
+        this.protectionFunctionForType = pProtectionFunctionForType;
+        this.enchantmentValue = pEnchantmentValue;
+        this.sound = pSound;
+        this.toughness = pToughness;
+        this.knockbackResistance = pKnockbackResistance;
+        this.repairIngredient = new LazyLoadedValue(pRepairIngredient);
+    }
+
+    public int getDurabilityForType(ArmorItem.Type pType) {
+        return (Integer)HEALTH_FUNCTION_FOR_TYPE.get(pType) * this.durabilityMultiplier;
+    }
+
+    public int getDefenseForType(ArmorItem.Type pType) {
+        return (Integer)this.protectionFunctionForType.get(pType);
+    }
+
+    public int getEnchantmentValue() {
+        return this.enchantmentValue;
+    }
+
+    public SoundEvent getEquipSound() {
+        return this.sound;
+    }
+
+    public Ingredient getRepairIngredient() {
+        return (Ingredient)this.repairIngredient.get();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public float getToughness() {
+        return this.toughness;
+    }
+
+    public float getKnockbackResistance() {
+        return this.knockbackResistance;
+    }
+
+    public String getSerializedName() {
+        return this.name;
+    }
+
+}
--- src/main/java/net/neonekiz/douglasbirthdaymod/item/ModItems.java
+++ src/main/java/net/neonekiz/douglasbirthdaymod/item/ModItems.java
@@ -1,19 +1,25 @@
 package net.neonekiz.douglasbirthdaymod.item;
 
+import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.neonekiz.douglasbirthdaymod.DouglasBirthDayMod;
+import net.neonekiz.douglasbirthdaymod.item.custom.BirthdayhatItem;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
 
             DeferredRegister.create(ForgeRegistries.ITEMS, DouglasBirthDayMod.MOD_ID);
 
-public static final RegistryObject<Item> PLUSHIE = ITEMS.register("plushie",
-        () -> new Item(new Item.Properties()));
+
+
+    public static final RegistryObject<Item> BIRTHDAY_HELMET = ITEMS.register("birthday_helmet",
+            () -> new BirthdayhatItem(ModArmorMaterials.BDAYHAT, ArmorItem.Type.HELMET, new Item.Properties()));
+
+
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
